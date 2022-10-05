ff9a208e6b09445ae33d114b8b22e8a607ca546b
Alice2506 <ralko84@mail.ru>
2022-10-05 12:43:28 +0300

    homework task 1 added

---
+++ Homework_seminar2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число от 100 до 999");    
+void GetSecond(){
+int number = int.Parse(Console.ReadLine()); 
+Console.WriteLine(number);
+    if (number > 99 && number < 1000)
+    {
+      Console.Write(number / 10 % 10);  
+    } 
+    else
+    {
+      Console.WriteLine("Введенное число не является трехзначным");
+    }
+}   
+GetSecond();
+
+
+
+
+
+//int number = 469;
+//Console.Write(number / 10 % 10);
